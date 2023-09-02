f6241c8942e415c2c44a3c4da2006c523bb0e73d
Raja Naveed <rajadahri11@gmail.com>
2023-09-02 10:46:22 +0500

    initial state

---
+++ src/data/experienceData.js
@@ -0,0 +1,21 @@
+const expData = [
+    {
+        id: "1",
+        org: "Freelance",
+        title: "Web Developer",
+        date: "Since 2022",
+        description:  
+            "As a Web Developer, I have successfully worked with the MERN stack and SQL gaining expertise in both frontend and backend development. I have also actively contributed to the testing of Node applications, ensuring high-quality deliverables. Utilizing Agile methodology, I effectively collaborate with teams to enhance productivity and ensure seamless project execution.",
+      },  
+      {
+        id: "2",
+        org: "Freelance",
+        title: "Graphic Designer",
+        date: "2018 - 2020",
+        description:  
+            "As a freelance graphic designer, I have worked independently and collaborated with clients to create visually appealing and impactful designs ideas, and deliver high-quality visual solutions that meet their needs. I have managed my schedule while ensuring timely project delivery and maintaining client satisfaction. ",
+      },  
+    
+];
+
+export default expData;
